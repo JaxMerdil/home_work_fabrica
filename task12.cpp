60451870443987a1bdced180b833441d56c635e7
SebastianVaell <sebastianvaell@yandex.ru>
2016-12-07 16:52:44 +0200

    Задача 12 v.2

--- task12.cpp
+++ task12.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int* swap(int* array, int size);
 
-void* ran(int* array, int size);
+int* randArr(int* array, int size);
+
+void printArr(int* array, int size);
 
 int main()
 {
     srand(time(NULL));
     int size = 12;
-    int* array = new int[size];
-    ran(array, size);
-    cout << endl;
-    swap(array, size);
-    for (int i = 0; i < size; ++i)
-    {
-        cout << array[i] << " ";
-    }
-    cout<< endl;
-    delete[]array;
+    int* mass = new int[size];
+    randArr(mass, size);
+    cout<<"Исходный массив: "<<endl;
+    printArr(mass,size);
+    swap(mass, size);
+    cout<<"Смена соседних четных и нечетных ячеек массива: "<<endl;
+    printArr(mass,size);
+    delete[] mass;
     return 0;
 }
 
@@ -36,13 +36,20 @@ int* swap(int* array, int size)
     return array;
 }
 
-void* ran(int* array, int size)
+int* randArr(int* array, int size)
 {
     for (int i = 0; i<size; i++)
     {
         array[i] = rand() % 13;
-        cout << array[i] << " ";
     }
-    cout << endl;
-    return 0;
+    return array;
+}
+
+void printArr(int* array, int size)
+{
+for (int i = 0; i < size; ++i)
+{
+    cout << array[i] << " ";
+}
+cout<< endl;
 }

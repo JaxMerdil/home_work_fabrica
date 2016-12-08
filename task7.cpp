6c23462f61917d0ce17fc0293c5a1606d8f1ade9
SebastianVaell <sebastianvaell@yandex.ru>
2016-12-08 23:23:36 +0200

    Задача 7.v.3

--- task7.cpp
+++ task7.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 #include <stdio.h>
+#include <cstring>
+
 using namespace std;
 
 
-void* cat(char *str1, const char *str2);
+void fooCat(char *str1, const char *str2);
 
 int main()
 {
-    char* str1, *str2;
-    str1 = new char[100];
-    str2 = new char[100];
-    memset(str1, 0, 100);
-    memset(str2, 0, 100);
-    gets_s(str1);
-    gets_s(str2,100);
-    cat(str1, str2);
+    int size=100;
+    char* str1= new char[size]();
+    char* str2= new char[size]();
     printf(str1);
     delete[]str1;
     delete[]str2;
     return 0;
 }
 
-void* cat(char *str1, const char *str2)
+void cat(char *str1, const char *str2)
 {
-while(*str1) str1++;
+str1 += strlen(str1);
 while(*str2)
 {
 *str1 = *str2;

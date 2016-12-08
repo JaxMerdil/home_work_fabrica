905eda5c440da877557ec459b8317f6cc7e13f01
SebastianVaell <sebastianvaell@yandex.ru>
2016-12-09 00:32:50 +0200

    Задача 7.v3

--- task7.cpp
+++ task7.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdio.h>
 #include <cstring>
-
 using namespace std;
 
 
@@ -12,13 +11,18 @@ int main()
     int size=100;
     char* str1= new char[size]();
     char* str2= new char[size]();
+    cout<<"Введите первую строку: ";
+    gets(str1);
+    cout<<"Введите вторую строку: ";
+    gets(str2);
+    fooCat(str1, str2);
     printf(str1);
     delete[]str1;
     delete[]str2;
     return 0;
 }
 
-void cat(char *str1, const char *str2){
+void fooCat(char *str1, const char *str2){
 str1 += strlen(str1);
 while(*str2){
 *str1 = *str2;

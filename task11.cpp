4c24724be5221516d5214060235a6f8f11908e50
Nikita <sebastianvaell@gmail.com>
2016-12-12 21:23:44 +0200

    Задача 11+cs+alg

--- task11.cpp
+++ task11.cpp
@@ -20,17 +20,13 @@ void DeleteFirstWordSymbols(const string& str1)
     char st2[str1.length()];
     for(int i=1;i<str1.length();i++)
     {
+        st2[j]=str1[i];
+        j++;
+
         if(str1[i] == ' ')
         {
-            st2[j]=str1[i];
             i++;
-            j++;
         }
-          else
-          {
-            st2[j]=str1[i];
-            j++;
-          }
     }
     printf(st2);
     cout<<endl;

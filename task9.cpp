a8c545981d72e5280fd38de3d32f601c8f87d3c3
SebastianVaell <sebastianvaell@yandex.ru>
2016-12-12 19:53:11 +0200

    Задача 9+codstyle

--- task9.cpp
+++ task9.cpp
@@ -3,28 +3,31 @@
 
 using namespace std;
 
-char* algOfCesar(char* st);
+char* AlgorithmCaesars(char* st);
 
 int main()
 {
-char srt[24];
-cout << "Alphabet (abcdefgh123456-0), offset 5: ";
-cin >> srt;
-cout << "Result:";
-cout << algOfCesar(srt) << endl;
-return 0;
+    char str[24];
+    cout << "Alphabet (abcdefgh123456-0), offset 5: ";
+    cin >> str;
+    cout << "Result:";
+    cout << AlgorithmCaesars(str) << endl;
+    return 0;
 }
 
-char* algOfCesar(char* st)
+char* AlgorithmCaesars(char* st)
 {
-char* cipherT = "abcdefgh123456-0";
-char* encrypt = new char[strlen(st)];
-for(int i = 0; i<strlen(st); i++)
-for(int j = 0; j<strlen(cipherT); j++)
-if(st[i] == cipherT[j])
-if((strlen(cipherT)-j) <= 5)
-encrypt[i] = cipherT[(j+5)%strlen(cipherT)];
-else
-encrypt[i] = cipherT[j+5];
-return encrypt;
+    char* cipherTable = "abcdefgh123456-0";
+    char* encrypt = new char[strlen(st)];
+        for(int i = 0; i<strlen(st); i++)
+        {
+            for(int j = 0; j<strlen(cipherTable); j++)
+            {
+                if(st[i] == cipherTable[j])
+                {
+                encrypt[i] = cipherTable[(j+5)%strlen(cipherTable)];
+                }
+            }
+        }
+    return encrypt;
 }

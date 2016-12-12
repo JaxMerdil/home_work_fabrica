1510a43efb5fdfaa211dfe98e61407e3276135cd
SebastianVaell <sebastianvaell@yandex.ru>
2016-12-12 21:05:25 +0200

    Задача 11+cs

--- task11.cpp
+++ task11.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <cstring>
-#include <string>
-#include <stdio.h>
 
 using namespace std;
 
-void delS1(string& st);
+void DelFSymAftSp(string& st); //Delete first symbol after the space
 
 int main()
 {
     string st1;
     printf("Enter the string: ");
     getline(cin, st1);
-    delS1(st1);
+    DelFSymAftSp(st1);
     return 0;
 }
 
-void delS1(string& str1)
+void DelFSymAftSp(string& str1)
 {
     int j=0;
     char st2[str1.length()];
-    for(int i=0;i<str1.length();i++)
+    for(int i=1;i<str1.length();i++)
     {
-        if (i == 0) continue;
-           if(str1[i] == ' ')
-           {
-		st2[j]=str1[i]; i++; j++;
-	   }
-           else
-	   {
-		  st2[j]=str1[i]; j++;
-	   }
+        if(str1[i] == ' ')
+        {
+            st2[j]=str1[i];
+            i++;
+            j++;
+        }
+          else
+          {
+            st2[j]=str1[i];
+            j++;
+          }
     }
     printf(st2);
     cout<<endl;

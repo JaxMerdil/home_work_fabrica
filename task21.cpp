061c5629ca4c5375b2e23382f01aff7b4204f05f
SebastianVaell <sebastianvaell@yandex.ru>
2016-12-22 16:58:01 +0200

    Задача 21+cs/fix

--- task21.cpp
+++ task21.cpp
@@ -11,9 +11,8 @@ private:
     float m_x;
     float m_y;
 public:
-    Shape() //Конструктор по умолчанию
+    Shape()  :m_x(0),m_y(0)      //Конструктор по умолчанию
     {
-        m_x=m_y=0;
         cout<<"Вызов конструктора по умолчанию базового класса"<<endl;
     }
     Shape(float x, float y) // Конструктор класса с параметрами
@@ -22,7 +21,7 @@ public:
         m_y=y;
         cout<<"Вызов параметризированого конструктора базового класса"<<endl;
     }
-    ~Shape() // Деструктор
+    virtual ~Shape() // Деструктор
     {
         cout << "Вызов деструктора Shape" << endl;
     }
@@ -54,7 +53,7 @@ private:
     float m_width;
     float m_height;
 public:
-    Rectangle(float w, float h)
+    Rectangle(int x, int y, float w, float h) : Shape(x, y)
     {
         m_width = w;
         m_height = h;
@@ -64,7 +63,7 @@ public:
     {
         cout << "Вызов деструктора Rectangle" << endl;
     }
-    float Square()
+    float Square() override
     {
         return m_width*m_height;
     }
@@ -78,7 +77,7 @@ private:
     float m_cathetusA;
     float m_cathetusB;
 public:
-    Triangle(float cA, float cB)
+    Triangle(int x, int y, float cA, float cB) : Shape(x, y)
     {
         m_cathetusA = cA;
         m_cathetusB = cB;
@@ -88,7 +87,7 @@ public:
     {
         cout << "Вызов деструктора Triangle" << endl;
     }
-    float Square()
+    float Square() override
     {
         return 0.5*m_cathetusA*m_cathetusB;
     }
@@ -101,7 +100,7 @@ class Circle: public Shape
 private:
     float m_radius;
 public:
-    Circle(float r)
+   Circle(int x, int y, int r) : Shape(x, y)
     {
         m_radius=r;
         cout<<"Вызов параметризированого конструктора класса Circle"<<endl;
@@ -110,7 +109,7 @@ public:
     {
         cout << "Вызов деструктора Rectangle" << endl;
     }
-    float Square()
+    float Square() override
     {
         return M_PI*m_radius*m_radius;
     }
@@ -134,11 +133,11 @@ int main()
 {
     const int nFigure=3;
     Shape* squareFigures[nFigure];
-    squareFigures[0] = new Rectangle(10, 10);
+    squareFigures[0] = new Rectangle(5, 5, 10, 10);
     cout << "S Rectangle: " << squareFigures[0]->Square() << endl;
-    squareFigures[1] = new Triangle(10, 15);
+    squareFigures[1] = new Triangle(5, 5, 10, 15);
     cout << "S Triangle: " << squareFigures[1]->Square() << endl;
-    squareFigures[2] = new Circle(10);
+    squareFigures[2] = new Circle(5, 5, 10);
     cout << "S Circle: " << squareFigures[2]->Square() << endl;
     PrintSquerOfShape(squareFigures,nFigure);
     return 0;

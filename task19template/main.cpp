acdc432556c2248ec4b98e771e061af63bda6c74
Nikita <sebastianvaell@gmail.com>
2017-01-04 19:25:33 +0200

    Задача 19+template/fix Tname

--- task19template/main.cpp
+++ task19template/main.cpp
@@ -1,8 +1,149 @@
 #include <iostream>
 #include <iomanip>
-#include <template.h>
+#include<cstring>
 
 using namespace std;
+
+//-------------------------------------------------------------------
+template<class T>
+void FillingMatrix(T* arr,const int size)
+{
+    for(int i  = 0; i < size; i++)
+    {
+        arr[i] = rand()%25+65;
+    }
+}
+template<>
+void FillingMatrix(char* arr,const int size)
+{
+    for(int i  = 0; i < size; i++)
+    {
+        arr[i] = rand()%25+65;
+    }
+}
+template<>
+void FillingMatrix(bool* arr,const int size)
+{
+    for(int i  = 0; i < size; i++)
+    {
+        arr[i] = rand()%2;
+    }
+}
+//-------------------------------------------------------------------
+
+template<class T>
+void PrintMatrix(T* arr, int size)
+{
+    for(int i  = 0; i < size; i++)
+    {
+        cout <<setw(4)<<arr[i];
+    }
+    cout << endl;
+}
+
+//-------------------------------------------------------------------
+
+template<class T>
+T IncrementNum(T value)
+{
+    return ++value;
+}
+template<>
+bool IncrementNum(bool value)
+{
+    if(value==1) return value=0;
+            else return value=1;
+}
+
+//-------------------------------------------------------------------
+
+template<class T>
+T MultipleTwo(T value)
+{
+
+    return value!=(T)74; // 74 = 'J'
+}
+template<>
+char MultipleTwo(char value)
+{
+
+    return value!='J';
+}
+template<>
+bool MultipleTwo(bool value)
+{
+
+    return value!=0;
+}
+
+//-------------------------------------------------------------------
+
+template<class T>
+T MultiplicationTwoArr(T arrA, T arrB)
+{
+     return  arrA+arrB;
+}
+
+template<>
+char MultiplicationTwoArr(char arrA, char arrB)
+{
+
+     return  arrA+arrB;
+}
+//-------------------------------------------------------------------
+
+template<class T>
+T *Map(T* arr, const int size, T (*MapFunc)(T))
+{
+    for(int i = 0; i < size; i++)
+    {
+        arr[i]  = MapFunc(arr[i]);
+    }
+    return arr;
+}
+
+//-------------------------------------------------------------------
+
+template<class T>
+T *Filter(T arr[], const int size, int& newSize, T (*FilterFunc)(T))
+{
+    int j = 0;
+    for(int i = 0; i < size; i++)
+    {
+        if( FilterFunc(arr[i]))
+        {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+    newSize = j;
+    return arr;
+}
+
+//-------------------------------------------------------------------
+
+template<class T>
+T Reduce(T* arr, const int size, T (*ReduceFupc)(T, T))
+{
+    T var = 0;
+    for(int i = 0; i < size; i++)
+    {
+      var = ReduceFupc(var,arr[i]);
+    }
+    return var;
+}
+template<>
+char Reduce(char* arr, const int size, char (*ReduceFupc)(char, char))
+{
+    char var = 0;
+    for(int i = 0; i < size; i++)
+    {
+      var = ReduceFupc(var,arr[i]);
+    }
+    return var;
+}
+
+//-------------------------------------------------------------------
 
 int main()
 {

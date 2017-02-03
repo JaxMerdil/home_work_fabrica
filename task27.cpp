5efb4b891dbf7b53233a430861a5d69d38ae6a85
Балеев Никита < >
2017-02-04 00:43:43 +0200

    Задача 27+fix

--- task27.cpp
+++ task27.cpp
@@ -2,9 +2,6 @@
 #include <iomanip>
 #include <bits/stdc++.h>
 #include <algorithm>
-//#include <map>
-//#include <set>
-//#include <unordered_map>
 
 using namespace std;
 
@@ -12,29 +9,24 @@ int main()
 {
     unordered_map<string, int> words_count;
     multimap<int, string> sortMap;
-    set <string> str = {"i", "or", "and", "that", "the", "of", "not", "to", "is", "a", "in", "with", "at", "an", "as",
+    unordered_set <string> str = {"i", "or", "and", "that", "the", "of", "not", "to", "is", "a", "in", "with", "at", "an", "as",
                         "for", "but", "his", "he", "are", "has", "who", "by", "about", "which" };
-    ifstream file("D:/test2.txt");
+    ifstream file("D:/test23.txt");
     if (!file.is_open())
     {
         cout << "File is not open!" << endl;
+        return 1;
     }
     string word;
     while (file >> word)
     {
         transform (word.begin(), word.end(), word.begin(), ::tolower);
-        if( binary_search(str.begin (), str.end(), word))
+        auto it = find(str.begin(), str.end(), word);
+        if (it != str.end())
         {
             continue;
         }
-        transform(word.begin(), word.end(), word.begin(), [](char ignore)
-        {
-            if( ispunct(ignore))
-            {
-                return '\0';
-            }
-            return ignore;
-        });
+        word.erase(remove_if(word.begin(), word.end(), [](const char& c){return ispunct(c);}), word.end());
         words_count[word]++;
     }
     for (auto it = words_count.begin(); it != words_count.end(); ++it)
